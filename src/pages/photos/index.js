10e79e3047a570331f962c7861ce2a353b6eb1d9
Max Crane <maxlccrane@gmail.com>
2018-10-17 23:03:14 -0700

    add ability to click on a photo to view it larger in a modal

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid'
 import _ from "lodash";
 import Button from "@material-ui/core/Button/Button"
 import Img from "gatsby-image";
+import Modal from '@material-ui/core/Modal';
 
 
 class PhotosPage extends Component {
@@ -32,7 +33,9 @@ class PhotosPage extends Component {
          allTag: allTag,
          tags,
          filteredImages: images,
-         images
+         images,
+         photoModalOpen: false,
+         photoModalIndex: 0
       };
    }
 
@@ -42,13 +45,24 @@ class PhotosPage extends Component {
       });
    }
 
+   photoClicked(index) {
+      this.setState({photoModalOpen: true, photoModalIndex: index});
+   }
+
+   handleClose() {
+      this.setState({photoModalOpen: false});
+   }
+
    render() {
+      const modalImageNode = this.state.filteredImages[this.state.photoModalIndex];
+      const fluidImage = modalImageNode
+            ? modalImageNode.node.fields.image.childImageSharp.fluid
+            : undefined;
+
       return (
          <Layout>
             <section className="section">
                <div className="container">
-
-
                   <div className="content" style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
                      {
                         this.state.tags.map((tag) =>
@@ -61,27 +75,47 @@ class PhotosPage extends Component {
                            </div>
                         )
                      }
-
                   </div>
 
                   <div className="content">
                      <Grid container justify="center" spacing={24}>
                         {
-                           this.state.filteredImages.map(imageNode => {
+                           this.state.filteredImages.map((imageNode, index) => {
                               const path = imageNode.node.frontmatter.image;
                               return (
-                                 <Img
-                                    key={path}
-                                    style={{margin: "5px"}}
-                                    alt={`path`}
-                                    fixed={imageNode.node.fields.image.childImageSharp.fixed}
-                                 />
-
+                                 <div
+                                    key={`${path}-container`}
+                                    className={'zoom-in'}
+                                    onClick={this.photoClicked.bind(this, index)}>
+                                    <Img
+                                       key={path}
+                                       style={{margin: "5px"}}
+                                       alt={`path`}
+                                       fixed={imageNode.node.fields.image.childImageSharp.fixed}
+                                    />
+                                 </div>
                               );
                            })
                         }
                      </Grid>
                   </div>
+
+
+                  <Modal
+                     aria-labelledby="simple-modal-title"
+                     aria-describedby="simple-modal-description"
+                     open={this.state.photoModalOpen}
+                     onClose={this.handleClose.bind(this)}
+                     className={"photo-modal"}
+                  >
+                     <div className={"photo-modal-content"}>
+                        <Img
+                           alt={`path`}
+                           fluid={fluidImage}
+                        />
+                     </div>
+                  </Modal>
+
                </div>
             </section>
          </Layout>
@@ -105,6 +139,10 @@ export const photosPageQuery = graphql`
                          childImageSharp {
                            fixed(width: 300, height: 300, quality: 100) {
                               ...GatsbyImageSharpFixed
+                           }
+                           fluid(maxWidth: 800, quality: 100) {
+                           # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
+                              ...GatsbyImageSharpFluid
                            }
                          }
                        }
